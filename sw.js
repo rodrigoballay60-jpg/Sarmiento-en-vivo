fcd5e1e1d6f373c9488bc53b78b1549578d8d38b
rodrigoballay60-jpg <rodrigoballay60@gmail.com>
2026-09-03 23:52:38 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -8,8 +8,8 @@ const SHELL_FILES = [
   './',
   './index.html',
   './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
+  './icon-192.png',
+  './icon-512.png',
 ];
 
 self.addEventListener('install', (event) => {
